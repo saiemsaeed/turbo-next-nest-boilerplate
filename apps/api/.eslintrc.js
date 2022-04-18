884fbef8406c7237828d2186ae2218f291363818
Saiem Saeed <saiem.saeed7@gmail.com>
2022-04-18 17:33:40 +0500

    Husky Configured tsconfig updated eslintrc updated jest testing scripts added

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
+  extends: 'config/eslint-preset',
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
